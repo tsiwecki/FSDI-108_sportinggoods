dc26a415be231404eb9a206d1522f4ee0ed5e84c
Thadeus Siwecki <thad.siwecki@gmail.com>
2021-05-31 21:51:33 -0500

    Fixed values

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -30,6 +30,7 @@ class NavBar extends Component {
             </li>
           </ul>
       
+          
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a className="dropdown-item" href="/#">Action</a></li>
             <li><a className="dropdown-item" href="/#">Another action</a></li>
